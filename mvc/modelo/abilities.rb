3d59dc026eb2c60cd29ef9bde3559de620f9e428
Agustin Parmisano <agustinparmisano@gmail.com>
2013-12-04 12:08:04 -0300

    agregado initialize de abilities y su clase test

--- mvc/modelo/abilities.rb
+++ mvc/modelo/abilities.rb
@@ -1,2 +1,11 @@
 class Abilities
+
+  def initialize(name, description, type, points, fighter)
+    @name = name
+    @description = description
+    @type = type
+    @points = points
+    @fighter = fighter
+  end
+
 end
---
+++ mvc/modelo/testModel/testAbilities.rb
@@ -0,0 +1,8 @@
+require "../abilities.rb"
+require 'test/unit'
+
+class TestAbilities < MiniTest::Unit::TestCase
+  def test_initialize
+
+  end
+end

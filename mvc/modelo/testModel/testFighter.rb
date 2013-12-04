59ed426fe63a93b841959dca3d8dd2d1ac1dc70c
Agustin Parmisano <agustinparmisano@gmail.com>
2013-12-04 12:58:20 -0300

    testFigther cambiado

--- mvc/modelo/testModel/testFighter.rb
+++ mvc/modelo/testModel/testFighter.rb
@@ -1,4 +1,15 @@
-require_relative '../fighters.rb'
+require "../fighter.rb"
+require 'minitest/autorun'
 
-a = Fighter.new('pepe')
-p a 
+class TestAbilities < MiniTest::Unit::TestCase
+  def setup
+    @fighter1 = Fighter.new('neim', 'descripchon', 'taip', 'pointz')
+    @fighter2 = Fighter.new('neim2', 'descripchon2', 'taip2', 'pointz2')
+  end
+  def test_initialize
+    assert_equal(@fighter1.name, 'neim')
+    assert_equal(@fighter1.description,'descripchon')
+    assert_equal(@fighter1.type,'taip')
+    assert_equal(@fighter1.points,'pointz')
+  end
+end

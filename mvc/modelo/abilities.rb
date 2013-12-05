0cfa152613c175b915e129356455f2a1ce0f9dee
Patricio <patricio@patricio-Lenovo-G470.(none)>
2013-12-05 10:21:17 -0300

    Abilities modificado y actualizado

--- mvc/modelo/abilities.rb
+++ mvc/modelo/abilities.rb
@@ -1,11 +1,50 @@
-class Abilities
+class Ability
 
-  def initialize(name, description, type, points, fighter)
+attr_accessor :name :points
+
+  def initialize(name, points)
     @name = name
-    @description = description
-    @type = type
     @points = points
-    @fighter = fighter
   end
 
 end
+
+#############################################################################################################################
+
+class HandHit < Ability
+
+	def fight_with(anAbility) 
+		anAbility response_to_handHit(self)
+	end
+
+end
+
+#############################################################################################################################
+
+class FootFit < Ability
+
+	def fight_with(anAbility) 
+		anAbility response_to_footHit(self)
+	end
+
+end
+
+#############################################################################################################################
+
+class Gun < Ability
+
+def fight_with(anAbility) 
+		anAbility response_to_gun(self)
+	end
+
+end	
+
+#############################################################################################################################
+
+class Especial < Ability
+
+	def fight_with(anAbility) 
+		anAbility response_to_especial(self)
+	end
+
+end	

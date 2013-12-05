a233c84d90c2430911fcca8b59e15a7a4f1896fc
Agustin Parmisano <agustinparmisano@gmail.com>
2013-12-05 13:57:54 -0300

    test faiter

--- mvcSimple/tests/testFighter.rb
+++ mvcSimple/tests/testFighter.rb
@@ -24,4 +24,27 @@ class TestFighter < MiniTest::Unit::TestCase
 
   end
 
+  def test_fight_result_tie
+    param1 ={ name: "Scorpio",
+             defense: 5, # > 0
+             attack: 3,  # > 0
+             life: 5 #cant be < 2
+    }
+
+    param2 ={ name: "SubZero",
+             defense: 3, # > 0
+             attack: 5,  # > 0
+             life: 5 #cant be < 2
+    }
+
+    @scorpio = Fighter.new(param1[:name],param1[:defense],param1[:attack],param1[:life])
+    @subzero = Fighter.new(param2[:name],param2[:defense],param2[:attack],param2[:life])
+
+    @andTheWinnerIs = @scorpio.fight(@subzero, 5)
+    assert_equal(@andTheWinnerIs, :empate)
+    rescue
+      puts 'Paso algo raro en test_fight_result_tie'
+    puts 'Passed'
+  end
+
 end

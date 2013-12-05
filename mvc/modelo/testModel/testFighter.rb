9f2bbe4f83b63b14b716a2e0173f87786edc39c9
Patricio <patricio@patricio-Lenovo-G470.(none)>
2013-12-05 09:39:50 -0300

    Asi se hace un test fighter

--- mvc/modelo/testModel/testFighter.rb
+++ mvc/modelo/testModel/testFighter.rb
@@ -2,8 +2,8 @@ require "../fighters.rb"
 require 'minitest/autorun'
 
 class TestFighter < MiniTest::Unit::TestCase
-  def setup
 
+  def test_initialize_with_complete_params
     param = {nameF: "exacto",
              lifeF: 2,
              pointsAF: 3,
@@ -12,9 +12,19 @@ class TestFighter < MiniTest::Unit::TestCase
     }
 
     @fighter1 = Fighter.new(param)
-    @fighter2 = Fighter.new(param)
+
+    assert_equal(@fighter1.name, 'exacto')
+    assert_equal(@fighter1.life, 2)
+    assert_equal(@fighter1.pointsA, 3)
+    assert_equal(@fighter1.pointsD, 3)
+    assert_equal(@fighter1.experience, 120)
   end
-  def test_initialize
+
+   def test_initialize_without_params
+    param = {}
+
+    @fighter1 = Fighter.new(param)
+
     assert_equal(@fighter1.name, 'exacto')
     assert_equal(@fighter1.life, 2)
     assert_equal(@fighter1.pointsA, 3)

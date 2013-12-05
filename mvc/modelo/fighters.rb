daa14d323562519b07b5f610dea19a77537f8615
Martín Aguilar <martinaguilar3190@gmail.com>
2013-12-05 10:25:22 -0300

    Le pongo revoque al Fighters

--- mvc/modelo/fighters.rb
+++ mvc/modelo/fighters.rb
@@ -1,6 +1,6 @@
 class Fighter
 
-    attr_accessor :name, :abilities, :life, :pointsA, :pointsD, :experience, :estrategyAttack, :estrategyDefense
+    attr_accessor :name, :abilities, :life, :experience, :strategyAttack, :strategyDefense
 
     def initialize(params)
         if params.size != 3 or !(params[:nameF].is_a? String) or !(params[:lifeF].is_a? Integer)
@@ -8,20 +8,57 @@ class Fighter
         else
           @name = params[:nameF]
           @abilities = []
-          setLife params[:lifeF]
-          @pointsA ||= 3
-          @pointsD ||= 3
+          life = params[:lifeF]
           @experience = params[:exp]
-          @estrategyAttack = []
-          @estrategyDefense = []
+          @strategyAttack = []
+          @strategyDefense = []
+          @abilitiesChosenD = []
+          @abilitiesChosenA = []
         end
     end
 
-    def setLife(life)
+    def life=(life)
       if life >= 2
-        @life = life
-      else raise
+          @life = life
+      else 
+          raise
       end
+    end
+
+    # Cada player tiene N habilidades, de ellas elige
+    # 3 habilidades de defensa y 3 de ataque
+    def pickAbilityForDefense(ability)
+        if @abilitiesChosenD.length < 3
+            @abilitiesChosenD << ability
+        else
+            raise
+        end
+    end
+
+    def pickAbilityForAttack(ability)
+        if @abilitiesChosenA.length < 3
+            @abilitiesChosenA << ability
+        else
+            raise
+        end
+    end
+
+    # De las 6 habilidades que cada player posee (3A y 3D), debe armar
+    # una secuencia de 5 ataques y 5 defensas
+    def addToDefenseStrategy(ability)
+        if @strategyDefense < 5
+            @strategyDefense << ability
+        else
+            raise
+        end
+    end
+
+    def addToAttackStrategy(ability)
+        if @strategyAttack.length < 5
+            @strategyAttack << ability
+        else
+            raise
+        end
     end
 
 end
